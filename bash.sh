408d44fccfb2028fd5a3456b9f68b4b5bad8855e
Seriki Walter Yakub <webapp4@outlook.com>
2026-07-07 19:37:16 +0100

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,11 @@
+git init
+
+git add .
+
+git commit -m "Initial repository scaffold"
+
+git branch -M main
+
+git remote add origin https://github.com/<your-username>/web4-marketplace.git
+
+git push -u origin main
